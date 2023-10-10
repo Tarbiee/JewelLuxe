970de8c6f12cd3bdd88559f7447a8148b6b09970
Tarbiee <dorcusstaby@gmail.com>
2023-10-10 15:14:14 +0300

    render the jewels

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+function renderJewelry(jewelry){
+    let card = document.createElement("div");
+    card.id = "card"
+    card.innerHTML=`
+     <h4 class="name">${jewelry.name}</h4>
+     <img src="${jewelry.image}" alt="img" class="image">
+     <p>${jewelry.description}</p>
+     <p class="">Material:${jewelry.material}</p>
+    <p class="price">Price:${jewelry.price}</p>`
+
+document.querySelector("#row").appendChild(card)
+}
+
+
+
+
+
+
+
+//fetch requests
+//
+
 fetch('http://localhost:3000/jewelry')
 .then(response => response.json())
 .then((jewelryData) =>{
-    console.log(jewelryData)
+    jewelryData.forEach(jewelry => renderJewelry(jewelry))
+     
 
        
     })
